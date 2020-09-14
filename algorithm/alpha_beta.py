f93e3bc10fe50d7f2e5f86048f5053e6f325561d
Braulio Lobo <brauliobaxon@gmail.com>
2020-09-14 01:24:33 -0300

    Imports solucionados

--- algorithm/alpha_beta.py
+++ algorithm/alpha_beta.py
@@ -1,46 +1,48 @@
 import math
+from algorithm.minmax import minmax
+from knight_chess.state import State
 
-def alpha_beta(state: State, maxDepth = 99999,alpha, beta):
-	if state.is_final() or maxDepth == 0:
-		return (None,state.value())
+def alpha_beta(state: State, alpha, beta, maxDepth=99999):
+    if state.is_final() or maxDepth == 0:
+        return (None, state.value())
 
-	if state.isMax:
-		maxValue = -math.inf
-		childs = []
-		for action in state.get_actions():
-			child = state.transition(action)
-			child.isMax = False
-			value = minmax(child,maxDepth-1)[1] # retorna el valor del estado
-			childs.append((action,value))
-			if value > maxValue:
-				maxValue = value
-			if value > alpha:
-				alpha = value
-			if beta <= alpha:
-				break
-			
-		for i in childs:
-			if i[1] == maxValue:
-				return i
+    if state.isMax:
+        maxValue = -math.inf
+        childs = []
+        for action in state.get_actions():
+            child = state.transition(action)
+            child.isMax = False
+            value = minmax(child, maxDepth-1)[1] # retorna el valor del estado
+            childs.append((action, value))
+            if value > maxValue:
+                maxValue = value
+            if value > alpha:
+                alpha = value
+            if beta <= alpha:
+                break
+            
+        for i in childs:
+            if i[1] == maxValue:
+                return i
 
-	else:
-		minValue = math.inf
-		childs = []
-		for action in state.get_actions():
-			child = state.transition(action)
-			child.isMax = True
-			value = minmax(child,maxDepth-1)[1] # retorna el valor del estado
-			childs.append((action,value))
-			if value < minValue:
-				minValue = value
-			if value < beta:
-				beta = value
-			if beta <= alpha:
-				break
-			
-		for i in childs:
-			if i[1] == minValue:
-				return i
+    else:
+        minValue = math.inf
+        childs = []
+        for action in state.get_actions():
+            child = state.transition(action)
+            child.isMax = True
+            value = minmax(child, maxDepth-1)[1] # retorna el valor del estado
+            childs.append((action, value))
+            if value < minValue:
+                minValue = value
+            if value < beta:
+                beta = value
+            if beta <= alpha:
+                break
+            
+        for i in childs:
+            if i[1] == minValue:
+                return i
 
-	return (None,0)
+    return (None,0)
 
--- algorithm/minmax.py
+++ algorithm/minmax.py
@@ -1,37 +1,38 @@
 import math
+from knight_chess.state import State
 
 def minmax(state: State, maxDepth = 99999):
-	if state.is_final() or maxDepth == 0:
-		return (None,state.value())
+    if state.is_final() or maxDepth == 0:
+        return (None, state.value())
 
-	if state.isMax:
-		maxValue = -math.inf
-		childs = []
-		for action in state.get_actions():
-			child = state.transition(action)
-			child.isMax = False
-			value = minmax(child,maxDepth-1)[1] # retorna el valor del estado
-			childs.append((action,value))
-			if value > maxValue:
-				maxValue = value
-			
-		for i in childs:
-			if i[1] == maxValue:
-				return i
+    if state.isMax:
+        maxValue = -math.inf
+        childs = []
+        for action in state.get_actions():
+            child = state.transition(action)
+            child.isMax = False
+            value = minmax(child, maxDepth-1)[1] # retorna el valor del estado
+            childs.append((action, value))
+            if value > maxValue:
+                maxValue = value
+            
+        for i in childs:
+            if i[1] == maxValue:
+                return i
 
-	else:
-		minValue = math.inf
-		childs = []
-		for action in state.get_actions():
-			child = state.transition(action)
-			child.isMax = True
-			value = minmax(child,maxDepth-1)[1] # retorna el valor del estado
-			childs.append((action,value))
-			if value < minValue:
-				minValue = value
-			
-		for i in childs:
-			if i[1] == minValue:
-				return i
+    else:
+        minValue = math.inf
+        childs = []
+        for action in state.get_actions():
+            child = state.transition(action)
+            child.isMax = True
+            value = minmax(child, maxDepth-1)[1] # retorna el valor del estado
+            childs.append((action, value))
+            if value < minValue:
+                minValue = value
+            
+        for i in childs:
+            if i[1] == minValue:
+                return i
 
-	return (None,0)
+    return (None, 0)
