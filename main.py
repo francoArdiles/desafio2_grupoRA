508860905ca2e7ee956c06a0337996c614f4dfb7
Braulio Lobo <brauliobaxon@gmail.com>
2020-09-21 10:26:08 -0300

    Entradas ya procesadas

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from algorithm import alpha_beta, minmax
 from knight_chess import State
+from copy import deepcopy as copy
+import sys
+import json
 
 if __name__ == '__main__':
-    state = State()
-    alpha_beta()
-    minmax()
+    # Obteniendo entrada del controlador
+    state_json = sys.argv[1]
+    state_json = state_json.replace(r'\"', '"')
+    state = json.loads(state_json)
+
+    # Generando el estado
+    state = State(state)
+    
+    # Copiando el estado
+    state_c1 = copy(state)
+    state_c2 = copy(state)
+
+    # Aplicando algoritmos de busqueda
+    final_state1 = alpha_beta(state_c1, float('inf'), float('-inf'))
+    final_state2 = minmax(state_c2)
 
