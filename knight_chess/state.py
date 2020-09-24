8c362b5972cb16711ec97596d43dab5d36032b8f
francoArdiles <franco.ardiles.a@gmail.com>
2020-09-23 22:35:13 -0300

    Se agregan metodos obtencion de posiciones de accion transicion validacion de accion
    Deben ser probadas aun

--- knight_chess/state.py
+++ knight_chess/state.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from .action import Action
+from copy import deepcopy
 
 WHITE = 1
 BLACK = 2
@@ -12,6 +13,17 @@ BLACK = 2
 # + + C + +
 # 6 + + + 1
 # + 7 + 0 +
+
+MOVEMENTS = {
+    0: (1, 2),
+    1: (2, 1),
+    2: (2, -1),
+    3: (1, -2),
+    4: (-1, -2),
+    5: (-2, -1),
+    6: (-2, 1),
+    7: (-1, 2),
+}
 
 
 class State:
@@ -26,20 +38,60 @@ class State:
         print('created empty state!')
 
     def transition(self, action: Action):
-        # elimina el caballo de su posición actual y lo pone en una nueva
-        # sin importar el estado al que llegue
+        # Copia del estado actual
+        new_state = deepcopy(self)
+
+        # identificacion de casilla actual y objetivo
+        start, end = self.get_movement_positions(action)
+
+        # identifica el elemento que se encuentra en la casilla objetivo
+        element = new_state.board[end]
+        # Limpia la posicion desocupada por el caballo
+        new_state.board[start] = np.nan
+
+        # Remueve elemento de la lista si es que existe
+        if element in self.enemy_knights:
+            new_state.enemy_knights.remove(element)
+        elif element in self.my_knights:
+            new_state.my_knights.remove(element)
+
+        new_state.board[end] = action.knight_id
         pass
 
+    def get_movement_positions(self, action):
+        movement = MOVEMENTS.get(action.knight_movement)
+        position = np.where(self.board == action.knight_id)
+        row, col = position[0][0], position[1][0]
+        new_row, new_col = row + movement[0], col + movement[1]
+
+        return (row, col), (new_row, new_col)
+
     def is_valid_action(self, action):
-        # Un caballo no puede saltar sobre otro del mismo tipo
-        # un caballo debe saltar dentro del tablero
-        pass
+        # identificacion de casilla actual y objetivo
+        player_id = int(action.knight_id/100)
+        # Esto no deberia ocurrir, por eso es excepcion
+        if player_id == 1:
+            if action.knight_id not in self.my_knights:
+                raise ValueError(f'Knight {action.knight_id} not in board')
+        else:
+            if action.knight_id not in self.enemy_knights:
+                raise ValueError(f'Knight {action.knight_id} not in board')
+
+        # Validacion de accion
+        start, end = self.get_movement_positions(action)
+        shape = self.board.shape
+        if end[0] < 0 or end[1] < 0:
+            return False
+        if end[0] > shape[0] or end[1] > shape[1]:
+            return False
+        if (self.board[end]/100) == player_id:
+            return False
+        return True
 
     def is_final(self):
         #retorna True si es final, False si no
         pass
 
-    @property
     def value(self):
         # Estados mejores:
         #   Cuando enemy knights sea menor al estado
