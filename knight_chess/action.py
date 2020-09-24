ee39ddaf65198aa9d318f4044ae68c1fe34a669a
francoArdiles <franco.ardiles.a@gmail.com>
2020-09-24 00:21:08 -0300

    fixed error dividiendo string e int

--- knight_chess/action.py
+++ knight_chess/action.py
@@ -4,16 +4,20 @@ import json
 class Action:
 
     def __init__(self, knight_id, movement):
-        self.knight_id = knight_id
+        self._knight_id = knight_id
         self.knight_movement = movement
 
     def send(self):
         result = {
-            "knight_id": self.knight_id,
+            "knight_id": self._knight_id,
             "knight_movement": self.knight_movement
         }
         print(json.dumps(result))
 
+    @property
+    def knight_id(self):
+        return int(self._knight_id)
+
     def __str__(self):
         return '"knight_id": {}, knight_movement: {}'.format(
-            self.knight_id, self.knight_movement)
+            self._knight_id, self.knight_movement)
